bdedad18f304c0439c9fe16ed29ff0cc95e93385
aivaz88 <aivaz1988@mail.ru>
2022-10-05 08:21:21 +0500

    Calculating average value

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2*. Написать программу, которая принимает от пользователя положительные числа и считает среднее значение этих чисел.
+// Ввод числе осуществляется до тех пор, пока пользовател не введёт -1. Ввод чисел и расчёт должен происходит в рекурсии
+
+Console.Clear();
+Console.WriteLine("Воведите целые положительные числа поочередно: ");
+AverageValue();
+
+
+void AverageValue(double sum = 0, double count = 0, double averageVal = 0)
+{
+    double number = double.Parse(Console.ReadLine()!);
+    if (number > -1) AverageValue(sum = sum + number, ++count);
+    else Console.WriteLine($"Среднее значение равно: {sum / count}");
+}
